847bca4672c47f85034effb9027a5b3b5b986c8e
NathanSFon <nathannaels276@gmail.com>
2021-05-10 09:45:18 -0300

    Update Consulta.java

--- src/Consulta.java
+++ src/Consulta.java
@@ -4,4 +4,22 @@ import Pessoa.paciente;
 public class Consulta {
     private medico med;
     private paciente pacien;
+
+    public Consulta(medico medi, paciente pa){
+        this.setMed(medi);
+        this.setPacien(pa);
+    }
+
+    public medico getMed() {
+        return med;
+    }
+    public paciente getPacien() {
+        return pacien;
+    }
+    public void setMed(medico med) {
+        this.med = med;
+    }
+    public void setPacien(paciente pacien) {
+        this.pacien = pacien;
+    }
 }
